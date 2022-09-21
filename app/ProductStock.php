7802402b14a0ba72c3741300cd8fd04cdcba900e
jain <vivekjain197@gmail.com>
2022-09-21 16:26:55 +0530

    stock model update

--- app/ProductStock.php
+++ app/ProductStock.php
@@ -7,7 +7,7 @@ use App\Models\ProductStock as Stock;
 class ProductStock extends Stock
 {
      protected $fillable = [
-        'user_id', 'variant', 'product_id', 'quantity', 'added_unit', 'reminder', 'user_id'
+        'user_id', 'variant', 'product_id', 'quantity', 'added_unit', 'reminder', 'user_id', 'price'
     ];
    
 }
